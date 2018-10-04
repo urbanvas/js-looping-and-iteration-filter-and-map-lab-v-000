91b4ebf331a099887e76b20b1dbaeccf7587eb43
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 22:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 function driversWithRevenueOver(dri, rev) {
   return dri.filter(el => {return el.revenue > rev})
 }
+
 function driversWithRevenueOver(dri, rev) {
   return dri.filter(el => {return el.revenue > rev}).map(el => el.name)
 }
 
-
-const drivers = [];
-drivers.push(
-  { name: 'Sally',   revenue: 400 },
-  { name: 'Annette', revenue: 200 },
-  { name: 'Jim',     revenue: 150 }
-);
+//
+// const drivers = [];
+// drivers.push(
+//   { name: 'Sally',   revenue: 400 },
+//   { name: 'Annette', revenue: 200 },
+//   { name: 'Jim',     revenue: 150 }
+// );
