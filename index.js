a3d0f8c29998beb1b4227ed668d8ba8a53530e0a
Neil Seejoor <nvseejoor@gmail.com>
2018-10-04 21:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 function driversWithRevenueOver(dri, rev) {
   const arr = []
-  return arr.push(dri.map(el => {
+  return dri.map(el => {
     el.revenue > rev
-  }))
+  })
 }
+
+let drivers = (
+  { name: 'Sally',   revenue: 400 },
+  { name: 'Annette', revenue: 200 },
+  { name: 'Jim',     revenue: 150 }
+);
